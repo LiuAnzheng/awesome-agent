52815b2665c4f36bd24deee551775f9afef150fb
liuanzheng <liuanzhengl@outlook.com>
2026-05-29 19:26:16 +0800

    feat(message): Message增加String方法

--- core/message.go
+++ core/message.go
@@ -3,6 +3,7 @@ package core
 import (
 	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -111,4 +112,106 @@ func NewFileContentPart(file *FileContent) ContentPart {
 func BuildBase64URL(data []byte, mimeType MIMEType) string {
 	base64Str := base64.StdEncoding.EncodeToString(data)
 	return fmt.Sprintf("data:%s;base64,%s", mimeType, base64Str)
+}
+
+const maxDataLen = 100
+
+func (m Message) String() string {
+	var b strings.Builder
+	b.WriteString("[")
+	b.WriteString(m.Role)
+	b.WriteString("]")
+
+	if m.ToolCallID != "" {
+		b.WriteString("[call_id=")
+		b.WriteString(m.ToolCallID)
+		b.WriteString("]")
+	}
+	if m.Name != "" {
+		b.WriteString("[name=")
+		b.WriteString(m.Name)
+		b.WriteString("]")
+	}
+
+	content := formatContent(m.Content)
+	if content != "" {
+		b.WriteString(" ")
+		b.WriteString(content)
+	}
+
+	if len(m.ToolCalls) > 0 {
+		b.WriteString(" [tool_calls:")
+		for i, tc := range m.ToolCalls {
+			if i > 0 {
+				b.WriteString(",")
+			}
+			b.WriteString(" ")
+			b.WriteString(tc.Function.Name)
+			b.WriteString("(")
+			args := tc.Function.Arguments
+			if len(args) > 200 {
+				args = args[:200] + "..."
+			}
+			b.WriteString(args)
+			b.WriteString(")")
+		}
+		b.WriteString("]")
+	}
+
+	return b.String()
+}
+
+func formatContent(c interface{}) string {
+	switch v := c.(type) {
+	case string:
+		return v
+	case []ContentPart:
+		parts := make([]string, 0, len(v))
+		for _, p := range v {
+			parts = append(parts, p.format())
+		}
+		return strings.Join(parts, " ")
+	case nil:
+		return ""
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+func (cp ContentPart) format() string {
+	switch cp.Type {
+	case Text:
+		return cp.Text
+	case Image:
+		url := ""
+		if cp.ImageURL != nil {
+			url = truncate(cp.ImageURL.URL, maxDataLen)
+		}
+		return "[image: " + url + "]"
+	case Audio:
+		data := ""
+		format := ""
+		if cp.InputAudio != nil {
+			data = truncate(cp.InputAudio.Data, maxDataLen)
+			format = cp.InputAudio.Format
+		}
+		return "[audio: " + format + " " + data + "]"
+	case File:
+		filename := ""
+		data := ""
+		if cp.File != nil {
+			filename = cp.File.Filename
+			data = truncate(cp.File.Data, maxDataLen)
+		}
+		return "[file: " + filename + " " + data + "]"
+	default:
+		return ""
+	}
+}
+
+func truncate(s string, maxLen int) string {
+	if len(s) <= maxLen {
+		return s
+	}
+	return s[:maxLen] + "..."
 }
